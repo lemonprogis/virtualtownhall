7edd7a4bd9971c3e8bd7320bbb3deb2f61381e82
lemonprogis <edward.briggler@gmail.com>
2015-01-14 22:26:32 -0600

    cleaned up html

--- poll/forms.py
+++ poll/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from models import *
 
@@ -7,6 +8,10 @@ class TownHallPollForm(ModelForm):
 		fields = ["question"]
 
 class TownHallResponseForm(ModelForm):
+	first_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'class':'form-control'}))
+	last_name = forms.CharField(required=False, widget=forms.TextInput(attrs={'class':'form-control'}))
+	email =forms.CharField(required=False, widget=forms.EmailInput(attrs={'class':'form-control'}))
+	answer = forms.CharField(required=True, widget=forms.Textarea(attrs={'class':'form-control'}))
 	class Meta:
 		model = TownHallResponse
 		fields = ["first_name", "last_name", "email", "answer","poll"]
